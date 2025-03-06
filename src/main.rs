6df8438f3806cf07612cddb1aa8812ad52f0165c
Donemmanuelo <emmanuelodon943@gmail.com>
2025-03-06 22:19:21 +0100

    just made some little changes

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let github_token = env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN not set");
     let repo_owner = env::var("GITHUB_REPOSITORY_OWNER").expect("GITHUB_REPOSITORY_OWNER not set");
     let repo_name = env::var("GITHUB_REPOSITORY_NAME").expect("GITHUB_REPOSITORY_NAME not set");
-    let pull_number = env::var("GITHUB_PULL_REQUEST_NUMBER").expect("GITHUB_PULL_REQUEST_NUMBER not set");               // Replace with your pull request number
+    let pull_number = env::var("GITHUB_PULL_REQUEST_NUMBER").expect("GITHUB_PULL_REQUEST_NUMBER not set");              
 
     // Parameters
     let max_threshold = env::var("INPUT_MAX_THRESHOLD").expect("INPUT_MAX_THRESHOLD not set");      // Maximum number for Fibonacci computation
-    let enable_fib = env::var("INPUT_ENABLE_FIB").expect("INPUT_ENABLE_FIB not set");       // Enable/disable Fibonacci computation and posting
+    let enable_fib = env::var("INPUT_ENABLE_FIB").expect("INPUT_ENABLE_FIB not set");      
     let max_threshold = max_threshold.trim().parse().expect("max_threshold not set");
     let enable_fib: bool = enable_fib.trim().parse().expect("enable_fib not set ");
     println!("GitHub Token: {}", &github_token);
